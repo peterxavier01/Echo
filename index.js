2fe2c5ef1c168ab8d24389be6e790de3598a6c71
Peter Uadiale <peteuadiale1@gmail.com>
2022-07-10 14:34:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,62 @@
+const textarea = document.querySelector("textarea"),
+  textToSpeechBtn = document.querySelector(".container button"),
+  voiceList = document.querySelector(".container select");
+
+let speechSynth = speechSynthesis;
+let isSpeaking = true;
+
+function voices() {
+  for (let voice of speechSynth.getVoices()) {
+    let selected =
+      voice.name === "Microsoft Zira - English (United States)"
+        ? "selected"
+        : ""; // Selecting Microsoft Zira as default
+    let option = ` <option value="${voice.name}" ${selected}>${voice.name} (${voice.lang})</option>`;
+    voiceList.insertAdjacentHTML("beforeend", option); // insert option tag before end of select element
+  }
+}
+
+speechSynth.addEventListener("voiceschanged", voices);
+
+function textToSpeech(text) {
+  let utterance = new SpeechSynthesisUtterance(text);
+  // set sppech voice to user selected voice if available
+  for (let voice of speechSynth.getVoices()) {
+    if (voice.name === voiceList.value) {
+      utterance.voice = voice;
+    }
+  }
+  speechSynth.speak(utterance); // speak the text
+}
+
+textToSpeechBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (textarea.value !== "") {
+    if (!speechSynth.speaking) {
+      // speak only when an utterance is not being spoken
+      textToSpeech(textarea.value);
+    }
+    if (textarea.value.length > 80) {
+      if (isSpeaking) {
+        speechSynth.resume();
+        isSpeaking = false;
+        textToSpeechBtn.innerText = "Pause Speech";
+      } else {
+        speechSynth.pause();
+        isSpeaking = true;
+        textToSpeechBtn.innerText = "Resume Speech";
+      }
+
+      // Check if an utterance is being spoken every 100ms.
+      // If not, change the button text
+      setInterval(() => {
+        if (!speechSynth.speaking && !isSpeaking) {
+          isSpeaking = true;
+          textToSpeechBtn.innerText = "Speak Text";
+        }
+      });
+    } else {
+      textToSpeechBtn.innerText = "Speak Text";
+    }
+  }
+});
